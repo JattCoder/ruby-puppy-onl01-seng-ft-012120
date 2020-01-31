04eb7b0f289e93bbf0e34eeee46282cec66d0de6
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-31 16:28:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all.push(name)
+  end
+  def self.all
+      p self
+  end
+  def self.clear_all
+    @@all = Array.new
+  end
+  def self.print_all
+    @@all.each do |dogname|
+      puts dogname
+    end
+  end
+  def name
+    @name
+  end
+  def save
+    @@all << p
+  end
+end
+
+pluto = Dog.new("Pluto")
+pluto.name
+pluto.save
